8def47b41e0ae89ae7451caf28b74414e07235d7
Aaron Boxer <boxerab@gmail.com>
2013-10-31 21:59:24 -0400

    native utilities now manages an arbitrary list of native libraries

--- src/main/java/org/codecCentral/imageio/generic/NativeUtilities.java
+++ src/main/java/org/codecCentral/imageio/generic/NativeUtilities.java
@@ -1,6 +1,9 @@
 package org.codecCentral.imageio.generic;
 
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -8,22 +11,49 @@ import java.util.logging.Logger;
 
 public class NativeUtilities {
 
-    private static final Logger LOGGER = Logger
-            .getLogger("org.openJpeg.imageio_openjpeg");
-
-    /** is OpenJpeg available on this machine?. */
-    private static boolean available;
-
-    private static boolean init = false;
+    private static final Logger LOGGER = Logger.getLogger("org.codecCentral.imageio.generic");
+    
+    private static Map<String, Boolean> available;
+    private static  Map<String, Boolean> initialized;
     
 /*----------------------------------------------------------------------*/
 
     private NativeUtilities() {
 
     }
+    
+    static
+    {
+    	available = new HashMap<String, Boolean>();
+    	initialized = new HashMap<String, Boolean>();
+    }
 
+    
+    private static String GenerateId(List<String> libraries)
+    {
+    	String rc = "";
+    	for (String library : libraries)
+    	{
+    		rc += library;
+    	}
+    	return rc;
+    }
 
+    private static boolean isTrue(Map<String, Boolean> map, List<String> libraries)
+    {
+    	String key = GenerateId(libraries);
+    	if (map.containsKey(key))
+    		return map.get(key);
+    	return false;
+	
+    }
+    private static void setTrue(Map<String, Boolean> map, List<String> libraries)
+    {
+    	String key = GenerateId(libraries);
+    	if (!map.containsKey(key))
+    		 map.put(key, true);
 
+    }
     /**
      * Returns <code>true</code> if the OpenJpeg native library has been loaded.
      * <code>false</code> otherwise.
@@ -31,30 +61,30 @@ public class NativeUtilities {
      * @return <code>true</code> only if the native library has been
      *         loaded.
      */
-    public static boolean areLibrariesAvailable(String libraryName,String jniLibraryName) {
-        loadLibraries(libraryName, jniLibraryName);
-        return available;
+    public synchronized static boolean areLibrariesAvailable(List<String> libraries) {
+        loadLibraries(libraries);
+        return isTrue(available, libraries);
     }
 
     /**
      * Forces loading of library libs.
      */
-    public synchronized static void loadLibraries(String libraryName,String jniLibraryName) {
-        if (init == false)
-            init = true;
-        else
+    public synchronized static void loadLibraries(List<String> libraries) {
+        if (isTrue(initialized, libraries))
             return;
+        setTrue(initialized, libraries);
         try {
-        	if (libraryName != null)
-				System.loadLibrary(libraryName);
-			if (jniLibraryName != null)
-				System.loadLibrary(jniLibraryName);
-            available = true;
+        	for (String library : libraries)
+        	{
+        		if (library != null)
+        			System.loadLibrary(library);
+        		
+        	}
+            setTrue(available,libraries);
         } catch (UnsatisfiedLinkError e) {
             if (LOGGER.isLoggable(Level.WARNING)){
             	 LOGGER.warning("Failed to load the native libs. Plugin disabled. " + e.toString());
             }
-            available = false;
         }
     }
     
